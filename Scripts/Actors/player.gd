43ea4ccab1bedc38baa2556bdfbd834ee409e7de
Oplexitie <arthur.cottreau@gmail.com>
2024-06-12 08:58:59 +0200

    fix player movement + cleanup

--- Scripts/Actors/player.gd
+++ Scripts/Actors/player.gd
@@ -15,7 +15,7 @@ var is_moving : bool = false
 var is_talking : bool = false
 var switch_walk : bool = false
 # Dialogue Related (talk direction)
-var cur_direction : Vector2 = Vector2(0,1)
+var cur_direction : Vector2 = Vector2.DOWN
 
 onready var Grid : TileMap = get_parent()
 onready var animtree : AnimationTree =  $AnimationTree
@@ -25,7 +25,7 @@ onready var walk_anim_length : float = $AnimationPlayer.get_animation("walk_down
 func _process(_delta):
 	input_dir_priority()
 	
-	# Allow inputs and movement if conditions are meet
+	# Allow movement if conditions are meet
 	if !is_moving and !is_talking:
 		# To Request dialogue
 		if Input.is_action_just_pressed("ui_accept"):
@@ -34,16 +34,15 @@ func _process(_delta):
 		var input_direction : Vector2 = direction_buffer()
 		
 		if input_direction:
-			cur_direction = input_direction
-			
 			# Checks if the next movement opportunity is possible :
+			cur_direction = input_direction
 			var target_position = Grid.request_move(self, input_direction)
 			
-			# If it's possible, move to target position
-			# If it ain't possible, play the idle animation
 			if target_position:
+				# If it's possible, move to target position
 				move_to(input_direction, target_position)
 			else:
+				# If it ain't possible, then set idle direction
 				animtree.set("parameters/StateMachine/Idle/blend_position", input_direction)
 
 func input_dir_priority():
@@ -53,23 +52,23 @@ func input_dir_priority():
 			var index : int = input_history.find(direction)
 			if index != -1:
 				input_history.remove(index)
+		
 		if Input.is_action_just_pressed(direction):
 			input_history.append(direction)
 
 func direction_buffer():
-	# If only one key is pressed, apply the latest input from the array to input_direction
-	# If two kesy are being pressed, check if the input are in opposite directions, if yes, then prevent movement
-	var history_size : int = input_history.size()
+	# Handles the movement direction depending on the inputs
+	var direction = Vector2()
+
+	if input_history.size():
+		for i in input_history:
+			direction += MOVEMENTS[i]
+
+		match(input_history.back()):
+			'ui_right', 'ui_left': if direction.x != 0: direction.y = 0
+			'ui_up', 'ui_down': if direction.y != 0: direction.x = 0
 	
-	if (history_size == 1):
-		return MOVEMENTS[input_history[history_size - 1]]
-	elif (history_size == 2):
-		match(input_history[history_size - 1]):
-			'ui_right', 'ui_left':
-				return Vector2(MOVEMENTS[input_history[history_size - 1]].x + MOVEMENTS[input_history[history_size - 2]].x, 0)
-			'ui_up', 'ui_down':
-				return Vector2(0, MOVEMENTS[input_history[history_size - 1]].y + MOVEMENTS[input_history[history_size - 2]].y)
-	return Vector2()
+	return direction
 
 func move_to(input_direction : Vector2, target_position : Vector2):
 	# Takes care of Animation Speed + Leg Switching (each step, the character swithes the leg they use)
